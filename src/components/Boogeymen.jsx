ab605ebdced3d7da993c58c924ab9e2b6a47c730
Louis <colletlouis@gmail.com>
2020-07-22 21:29:13 +0200

    Centered and title

--- src/components/Boogeymen.jsx
+++ src/components/Boogeymen.jsx
@@ -13,14 +13,22 @@ const Boogeymen = () => {
   }, [boogeyDatas]);
 
   return (
-    <div>
-      {boogeyDatas &&
-        boogeyDatas.map((killer) => (
-          <DisplayBoogeyman id={killer.id} name={killer.name} url={killer.avatar} />
-        ))}
-      <Link to="/">
-        <button type="button">Home</button>
-      </Link>
+    <div className="container">
+      <div className="row">
+        <Link to="/" className="col-1">
+          <button type="button">Home</button>
+        </Link>
+        <h1 className="col-10 text-center">Title</h1>
+        <div className="col-1" />
+      </div>
+      <div className="row">
+        {boogeyDatas &&
+          boogeyDatas.map((killer) => (
+            <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 ">
+              <DisplayBoogeyman id={killer.id} name={killer.name} url={killer.avatar} />
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
--- src/components/DisplayBoogeyman.jsx
+++ src/components/DisplayBoogeyman.jsx
@@ -4,12 +4,20 @@ import PropTypes from 'prop-types';
 const DisplayBoogeyman = (props) => {
   const { url, name } = props;
   return (
-    <div>
-      <div className="card" style={{ width: '18rem' }}>
-        <img src={url} className="card-img-top" alt={name} />
-        <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-        </div>
+    <div className="card mx-auto" style={{ width: '18rem' }}>
+      <img src={url} className="card-img-top" style={{ height: '30rem' }} alt={name} />
+      <div className="card-body">
+        <h5
+          className="card-title text-center"
+          style={{
+            'border-right': 'solid 1px #ddd ',
+            'border-left': 'solid 1px #ddd ',
+            'border-bottom': 'solid 1px #ddd',
+            'border-radius': '5px',
+          }}
+        >
+          {name}
+        </h5>
       </div>
     </div>
   );
