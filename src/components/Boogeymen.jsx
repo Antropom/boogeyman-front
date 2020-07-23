8dd70064a16c275c01131b8d0cfd6b31d4d57c7a
Louis <colletlouis@gmail.com>
2020-07-23 15:35:47 +0200

    Ordering done

--- src/components/Boogeymen.jsx
+++ src/components/Boogeymen.jsx
@@ -5,6 +5,7 @@ import DisplayBoogeyman from './DisplayBoogeyman';
 
 const Boogeymen = () => {
   const [boogeyDatas, setBoogeyDatas] = useState([]);
+  const [orderedKillers, setOrderedKillers] = useState([]);
 
   useEffect(() => {
     if (boogeyDatas.length === 0) {
@@ -12,10 +13,24 @@ const Boogeymen = () => {
     }
   }, [boogeyDatas]);
 
+  useEffect(() => {
+    if (boogeyDatas.length > 0 && orderedKillers.length === 0) {
+      const ordered = [];
+      const arrLength = boogeyDatas.length;
+      for (let i = 0; i < arrLength; i++) {
+        const index = boogeyDatas.reduce((a, b, i) => (boogeyDatas[a].votes < b.votes ? i : a), 0);
+        ordered.push(boogeyDatas[index]);
+        boogeyDatas.splice(index, 1);
+      }
+      console.log(ordered);
+      setOrderedKillers(ordered);
+    }
+  }, [boogeyDatas]);
+
   return (
-    <div className="container-fluid bg-dark mx-auto">
+    <div className="container bg-dark mx-auto">
       <div className="row">
-        <Link to="/" className="col-1 offset-1 mt-3 nodeco anim">
+        <Link to="/" className="col-1 mt-3 nodeco anim">
           <h3 className="text-center mx-auto ncolor perma text-uppercase">home</h3>
         </Link>
       </div>
@@ -24,20 +39,30 @@ const Boogeymen = () => {
           who&apos;s the best ?
         </h1>
       </div>
-      <div className="container">
-        <div className="row bg-dark">
-          {boogeyDatas &&
-            boogeyDatas.map((killer) => (
-              <div key={killer.id} className="col-sm-12 col-md-6 col-lg-4 col-xl-3">
-                <DisplayBoogeyman
-                  id={killer.id}
-                  name={killer.name}
-                  url={killer.avatar}
-                  votes={killer.votes}
-                />
-              </div>
-            ))}
-        </div>
+      {/* <div className="row">
+        {orderedKillers && (
+          <div key={orderedKillers[0].id} className="col-sm-12 col-md-6 col-lg-4 col-xl-3">
+            <DisplayBoogeyman
+              id={orderedKillers[0].id}
+              name={orderedKillers[0].name}
+              url={orderedKillers[0].avatar}
+              votes={orderedKillers[0].votes}
+            />
+          </div>
+        )}
+      </div> */}
+      <div className="row bg-dark">
+        {orderedKillers &&
+          orderedKillers.map((killer) => (
+            <div key={killer.id} className="col-sm-12 col-md-6 col-lg-4 col-xl-3">
+              <DisplayBoogeyman
+                id={killer.id}
+                name={killer.name}
+                url={killer.avatar}
+                votes={killer.votes}
+              />
+            </div>
+          ))}
       </div>
     </div>
   );
