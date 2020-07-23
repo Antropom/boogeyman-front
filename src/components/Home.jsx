ead42932826717e5d2416268a2ebf61c808ea242
Louis <colletlouis@gmail.com>
2020-07-23 12:16:45 +0200

    Homepage done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import intro from '../video/intro.mp4';
 
 const Home = () => {
   return (
     <div>
-      <Link to="/boogeymen">
-        <button type="button" className="btn btn-danger">
-          Next
-        </button>
-      </Link>
-      <Link to="/admin">
-        <button type="button" className="btn btn-danger">
-          Admin
-        </button>
-      </Link>
+      <video className="intro-video" autoPlay loop>
+        <source src={intro} type="video/mp4" />
+      </video>
+      <div className="container">
+        <div className="row mt-3">
+          <Link
+            to="/admin"
+            className="col-3 offset-8 col-sm-3 offset-sm-9 col-md-2 offset-md-10 col-lg-1 offset-lg-10 mb-5 nodeco"
+          >
+            <h3 className="text-center mx-auto ncolor perma text-uppercase">admin</h3>
+          </Link>
+        </div>
+        <div className="row">
+          <h1 className="col-12 text-center mx-auto ncolor perma big-title text-uppercase mb-5 mt-5">
+            {' '}
+            Who&apos;s the best Boogeyman
+          </h1>
+        </div>
+        <div className="row">
+          <Link to="/boogeymen" className="col-12 mt-5 nodeco">
+            <h2 className="text-center ncolor perma  text-uppercase smtitle">Vote now !</h2>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
